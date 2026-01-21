2281180e12202d9f4eb4aa178120a9015b22e601
ssun <yellowpurplebird@gmail.com>
2026-01-21 15:08:05 -0800

    code syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,45 @@
 // .eleventy.js
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const dayjs = require("dayjs");
-const Prism = require("prismjs");
-// Load languages you need (add more as needed)
-require("prismjs/components/prism-javascript");
-require("prismjs/components/prism-css");
-require("prismjs/components/prism-markup");
-require("prismjs/components/prism-bash");
-require("prismjs/components/prism-python");
-require("prismjs/components/prism-json");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets");
 
   // Add Syntax Highlighting plugin
-  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(syntaxHighlight, {
+
+    // Line separator for line breaks
+    lineSeparator: "\n",
+
+    // Change which Eleventy template formats use syntax highlighters
+    templateFormats: ["*"], // default
+
+    // Use only a subset of template types (11ty.js added in v4.0.0)
+    // templateFormats: ["liquid", "njk", "md", "11ty.js"],
+
+    // init callback lets you customize Prism
+    init: function({ Prism }) {
+      Prism.languages.myCustomLanguage = { /* … */ };
+    },
+
+    // Added in 3.1.1, add HTML attributes to the <pre> or <code> tags
+    preAttributes: {
+      tabindex: 0,
+
+      // Added in 4.1.0 you can use callback functions too
+      "data-language": function({ language, content, options }) {
+        return language;
+      }
+    },
+    codeAttributes: {},
+  });
+
+  // Added in 5.0.0, throw errors on invalid language names
+  errorOnInvalidLanguage: false,
 
   // Add dayjs date filter
   eleventyConfig.addFilter("date", function(date, format) {
     return dayjs(date).format(format);
-  });
-
-  // Add PrismJS syntax highlighting filter
-  eleventyConfig.addFilter("prism", function(code, language) {
-    if (!language) language = "markup";
-    if (Prism.languages[language]) {
-      return Prism.highlight(code, Prism.languages[language], language);
-    }
-    return code; // Return unhighlighted if language not found
   });
 
   return {
